6053549b1b76de75771f5ad287c472e31bf49696
meghpatil21 <megh.after18@gmail.com>
2022-10-22 15:17:38 +0530

    Implemented Temp Conversion

--- NUnitTest/Program.cs
+++ NUnitTest/Program.cs
@@ -4,44 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter month number: ");
-            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Select Conversion:\n 1.Celsius to Fahrenheit:\n 2.Fahrenheit to Celsius:");
+            int opt = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter Date: ");
-            int d = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Year: ");
-            int y = Convert.ToInt32(Console.ReadLine());
-
-            int y0, x, m0, d0;
-
-            y0 = y - (14 - m) / 12;
-            x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
-            m0 = m + 12 * ((14 - m) / 12) - 2;
-            d0 = (d + x + (31 * m0 / 12)) % 7;
-
-            switch (d0)
+            switch (opt)
             {
-                case 0:
-                    { Console.WriteLine("Sunday"); }
-                    break;
                 case 1:
-                    { Console.WriteLine("Monday"); }
+                    {
+                        Console.WriteLine("Enter Temp Value in Celsius: ");
+                        float f = Convert.ToSingle(Console.ReadLine());
+
+                        float result = (f * 9 / 5) + 32;
+                        Console.WriteLine($"{f} Celsius = {result} Fahrenheit");
+                    }
                     break;
                 case 2:
-                    { Console.WriteLine("Tuesday"); }
+                    {
+                        Console.WriteLine("Enter Temp Value in Fahrenheit: ");
+                        float f = Convert.ToSingle(Console.ReadLine());
+
+                        float result = (f - 32) * 5 / 9;
+                        Console.WriteLine($"{f} Fahrenheit = {result} Celsius");
+                    }
                     break;
-                case 3:
-                    { Console.WriteLine("Wednesday"); }
-                    break;
-                case 4:
-                    { Console.WriteLine("Thursday"); }
-                    break;
-                case 5:
-                    { Console.WriteLine("Friday"); }
-                    break;
-                case 6:
-                    { Console.WriteLine("Saturday"); }
+                default:
+                    Console.WriteLine("Invalid option");
                     break;
             }
         }
