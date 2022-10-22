bb86d49f11069de3ab6c1a9d38843947d166c40e
meghpatil21 <megh.after18@gmail.com>
2022-10-22 15:02:38 +0530

    Welcome Msg Added

---
+++ NUnitTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NUnitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NUnit Testing!");
+        }
+    }
+}
